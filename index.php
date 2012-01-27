f6c6d62057510bc9e8835b5fe51a34c03bf750ef
root <root@ubuntu.(none)>
2012-01-27 13:36:10 -0700

    Clone of original placeholder site

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>Redditors Without Borders</title>
+		<style>
+			a { text-decoration: none; color:#000000; }
+			a:hover { text-decoration: underline; }
+		</style>
+	</head>
+	<body>
+		<div align='center' style='padding-top:50px'>
+			<h1 style='color:#ff4400;'>Redditors Without Borders</h1>
+			<h2 style='color:#ff4400;'>We are developing our site, please <a href="http://www.Reddit.com/r/RWB">Join Us at Reddit</a></h2>
+			<img class="logo" src="images/reddit.jpg" />
+			<h2 style='color:#ff4400;'>BE THE CHANGE YOU WANT TO SEE</h2>
+		</div>	
+		<div class="social">
+			<a href="http://www.facebook.com/pages/Redditors-Without-Borders/353038971381560"><img class="logo" src="images/facebook.jpg" /></a>
+				
+			<a href="https://twitter.com/#!/RedditWB"><img class="logo" src="images/twitter.jpg" /></a>
+		</div>
+	</body>
+</html>
