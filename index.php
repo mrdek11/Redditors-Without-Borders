41041cbd3faeabe0f266ca71ad0c951ff5dd4160
Derek Peterson <dmp013@gmail.com>
2012-01-27 20:36:24 -0700

    Did a bit of styling, going for more of a redditgifts page feel

--- index.php
+++ index.php
@@ -25,15 +25,15 @@ if(isset($_POST['form'])){
 	<body>
 		<?php $rwb->displayHeader() ?>
 		<div align='center'>
-			<h1 style='color:#7CB5E5;'>Redditors Without Borders</h1>
-			<img src='images/logo-big.png' />
-			<h2 style='color:#7CB5E5;'>We are developing our site, please <a href="http://www.Reddit.com/r/RWB">Join Us at Reddit</a></h2>
+			<h1>redditors without borders</h1><br /><br />
+			<h2><a href="http://www.Reddit.com/r/RWB">We are developing our site. Meanwhile, please join us on Reddit</a></h2>
+
 		</div>
 		<br /><br /><br /><br />
 		<div class="social" align='center'>
-			<a href="http://www.facebook.com/pages/Redditors-Without-Borders/353038971381560"><img class="logo" src="images/facebook.jpg" /></a>
+			<a href="http://www.facebook.com/pages/Redditors-Without-Borders/353038971381560" target='_BLANK'><img class="logo" src="images/facebook.jpg" /></a>
 				
-			<a href="https://twitter.com/#!/RedditWB"><img class="logo" src="images/twitter.jpg" /></a>
+			<a href="https://twitter.com/#!/RedditWB" target='_BLANK'><img class="logo" src="images/twitter.jpg" /></a>
 		</div>
 	</body>
 </html>
--- lib/rwb-class.php
+++ lib/rwb-class.php
@@ -12,14 +12,21 @@ function displayNews(){
 }	
 function displayHeader(){
 		echo "<style>
-                       a { text-decoration: none; color:#000000; }
-                       a:hover { text-decoration: underline; }
+                       a { text-decoration: none; color:#5a83c3; }
+                       a:hover { color:#F26019; }
+		       body{ font-family:'Trebuchet MS', Trebuchet; }
                       </style> ";
 		echo "<table width='100%'>";
 		
 		echo "<tr>
-			<td>
-				<a href='index.php'><img src='images/logo.png' border='0' /></a>
+			<td style='padding-left:40px;padding-top:50px;'>
+				<a href='index.php'><img src='images/logo.png' border='0' /></a><br /><br />
+				<table>
+					<tr>
+						<td style='padding-left:20px;font-size:20px;'><a href='index.php'>home</a></td>
+						<td style='padding-left:20px;font-size:20px;'><a href='#'>causes</a></td>
+					</tr>
+				</table>
 			</td>
 		        <td valign='top' align='right'>";
 		if(!isset($_SESSION['username'])){
@@ -57,7 +64,7 @@ function displayHeader(){
                 	echo "<a href='profile.php'>Profile</a> | <a href='index.php?logout'>Logout {$_SESSION['username']}</a>";
                 }
                 echo "</div>";
-		echo "<div id='loginForm' style='display:none;s-index:10;position:absolute;top:25px;right:20px;background-color:white;'>
+		echo "<div id='loginForm' style='display:none;s-index:10;position:absolute;top:30px;right:20px;background-color:white;'>
 			<form action='index.php' method='post'>
 			Username: <input type='text' name='username' /><br />
 			Password: <input type='password' name='password' /><br />
@@ -65,7 +72,7 @@ function displayHeader(){
 			<input type='submit' value='Login' />
 			</form>
 		      </div>";
-		echo "<div id='registrationForm' style='display:none;s-index:10;position:absolute;top:25px;right:20px;background-color:white;'>
+		echo "<div id='registrationForm' style='display:none;s-index:10;position:absolute;top:30px;right:20px;background-color:white;'>
                         <form action='index.php' method='post'>
                         Username: <input type='text' name='username' /><br />
                         Password: <input type='password' name='password' /><br />
