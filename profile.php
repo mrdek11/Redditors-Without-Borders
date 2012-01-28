9766620563189f6761fbb26e73ebf8dfb5167fe1
Derek Peterson <dmp013@gmail.com>
2012-01-27 20:44:05 -0700

    Added support for Rank in profile view

--- profile.php
+++ profile.php
@@ -13,13 +13,15 @@ $rwb->displayHeader();
 $query = Query("select * from Users where id='{$_SESSION['userID']}}' limit 1;");
 $user = mysql_fetch_object($query);
 
-echo "<table>";
-echo "<form action='profile.php' method='post'>";
-echo "<tr><td style='font-weight:bold'>Username:</td><td>$user->Username</td></tr>";
-echo "<tr><td style='font-weight:bold'>Email:</td><td><input type='text' name='email' value=\"$user->Email\"</td></tr>";
-echo "<tr><td valign='top' style='font-weight:bold'>Assets:</td><td><textarea name='assets' rows=5 cols=40>$user->Assets</textarea></td></tr>";
-echo "<tr><td><input type='submit' value='Update' /></td></tr>";
-echo "</form>";
-echo "</table>";
+echo "<div align='center'>";
+	echo "<table>";
+		echo "<form action='profile.php' method='post'>";
+			echo "<tr><td style='font-weight:bold'>Username:</td><td>$user->Username (<b>$user->Rank</b>)</td></tr>";
+			echo "<tr><td style='font-weight:bold'>Email:</td><td><input type='text' name='email' value=\"$user->Email\"</td></tr>";
+			echo "<tr><td valign='top' style='font-weight:bold'>Assets:</td><td><textarea name='assets' rows=5 cols=40>$user->Assets</textarea></td></tr>";
+			echo "<tr><td><input type='submit' value='Update' /></td></tr>";
+		echo "</form>";
+	echo "</table>";
+echo "</div>";
 
 ?>
