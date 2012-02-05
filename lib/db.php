e2c52aa7fb8c71080f41dccb114914b5736f227f
mrdek11 <dmp013@gmail.com>
2012-02-05 04:51:27 +0000

    Added Rank to the sql file

--- lib/db.php
+++ lib/db.php
@@ -1,6 +1,6 @@
 <?
-mysql_connect("localhost","rwb","rwbpass");
-mysql_select_db("rwb");
+mysql_connect("localhost","rwb","rwbpass") or die(mysql_error());
+mysql_select_db("rwb") or die(mysql_error());
 function Query($query){
 	$query = mysql_query($query) or die(mysql_error());
 	return $query;
--- lib/rwb.sql
+++ lib/rwb.sql
@@ -55,6 +55,7 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `Username` varchar(40) NOT NULL,
   `Password` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
+  `Rank` varchar(10) NOT NULL,
   `Registered` int(11) NOT NULL COMMENT 'timestamp',
   `LastLogin` int(11) NOT NULL COMMENT 'timestamp',
   `Assets` text,
