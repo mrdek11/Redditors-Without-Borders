2b517b55a497042f610f392f4299df4f9140df8e
mrdek11 <dmp013@gmail.com>
2012-03-26 17:22:01 -0600

    Accidentally forgot to update table structure with last commit.  Fixed.

--- lib/rwb.sql
+++ lib/rwb.sql
@@ -1,3 +1,5 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
 --
 -- Database: `rwb`
 --
@@ -9,14 +11,15 @@
 --
 
 CREATE TABLE IF NOT EXISTS `Causes` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Title` varchar(255) NOT NULL,
   `Detail` text NOT NULL,
   `Goal` decimal(10,2) NOT NULL,
-  `Created` int(11) NOT NULL COMMENT 'timestamp',
-  `GoalMet` int(11) NOT NULL COMMENT 'timestamp',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `Created` int(11) NOT NULL DEFAULT '0' COMMENT 'timestamp',
+  `GoalMet` int(11) NOT NULL DEFAULT '0' COMMENT 'timestamp',
+  `Image` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -25,11 +28,11 @@ CREATE TABLE IF NOT EXISTS `Causes` (
 --
 
 CREATE TABLE IF NOT EXISTS `News` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Title` varchar(255) NOT NULL,
   `Body` text NOT NULL,
   `Timestamp` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
@@ -51,14 +54,14 @@ CREATE TABLE IF NOT EXISTS `Subscriptions` (
 --
 
 CREATE TABLE IF NOT EXISTS `Users` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Username` varchar(40) NOT NULL,
   `Password` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
-  `Rank` varchar(10) NOT NULL,
+  `Rank` varchar(10) NOT NULL DEFAULT 'Donor',
   `Registered` int(11) NOT NULL COMMENT 'timestamp',
   `LastLogin` int(11) NOT NULL COMMENT 'timestamp',
   `Assets` text,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
